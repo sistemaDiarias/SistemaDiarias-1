d254a700ee2abb5e557c80c4fc0e8454a3e68199
root <root@ubuntu-pc>
2016-11-11 02:50:25 -0300

    Retirada do bootstrap remoto e adicionado o local

--- index.php
+++ index.php
@@ -4,16 +4,15 @@
   <title>Pagina inicial</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/estilo.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div class="container text-center">
-    
-    <p>Ir para <a href="cadastro_servidores.php">Cadastro de servidores</a></p>
+<div class="container text-center">    
+    <p>Ir para <a href="cadastro_servidores.php">Cadastro de servidores</a> </p>
 </div>
 
 </body>
