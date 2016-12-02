18168fde862e66c431e4c6d5e3022cd8468e8b22
root <root@ubuntu-pc>
2016-12-02 01:03:25 -0300

    Melhoria de reaproveitamento no código

--- DAO/CargoDAO.php
+++ DAO/CargoDAO.php
@@ -10,20 +10,14 @@ require_once('classes/Cargo.php');
 
 class CargoDAO
 {
-    private $conexao;
-
-    function __construct($conexao)
-    {
-        $this->conexao = $conexao;
-    }
 
     function inserir(Cargo $cargo)
     {
         try {
             $query = "insert into cargo(nome,id_perfil_diaria) values('{$cargo->getNome()}','{$cargo->getPerfilDiaria()->getId()}')";
-
-            mysqli_query($conexao, $query);
-            
+            $dao = new DAO();
+            $conexao = $dao->getConexao();
+            $resultado = $conexao->query($query);            
             return true;
         } catch (Exception $ex) {
             echo $ex->getMessage();
@@ -34,46 +28,46 @@ class CargoDAO
     function buscarPorId($id)
     {
         $query = "select * from cargo where id = '{$id}'";
+        $dao = new DAO();
+        $conexao = $dao->getConexao();
+        $resultado = $conexao->query($query);
+        $cargo = $resultado->fetch_all(MYSQLI_ASSOC);
 
-        $resultado = mysqli_query($conexao, $query);
-
-        $cargos = array();
-
-        while($cargo = mysqli_fetch_assoc($resultado))
-        {
-            array_push($cargos,$cargo);
+        if(count($cargo)==0){
+            $dao->fecharConexao();
+            return NULL;
         }
-
-        return $cargo;
+        $dao->fecharConexao();
+        return $cargo[0];
     }
     function buscarPorNome($nome)
     {
         $query = "select * from cargo where nome = '{$nome}'";
+        $dao = new DAO();
+        $conexao = $dao->getConexao();
+        $resultado = $conexao->query($query);
+        $cargo = $resultado->fetch_all(MYSQLI_ASSOC);
 
-        $resultado = mysqli_query($conexao, $query);
-
-        $cargos = array();
-
-        while($cargo = mysqli_fetch_assoc($resultado))
-        {
-            array_push($cargos,$cargo);
+        if(count($cargo)==0){
+            $dao->fecharConexao();
+            return NULL;
         }
-
-        return $cargo;
+        $dao->fecharConexao();
+        return $cargo[0];
     }
     function listarTodos()
     {
-        $query = "select * from cargo";
+        $query = "select * from cargo ORDER BY nome ASC";
+        $dao = new DAO();
+        $conexao = $dao->getConexao();
+        $resultado = $conexao->query($query);
+        $cargo = $resultado->fetch_all(MYSQLI_ASSOC);
 
-        $resultado = mysqli_query($this->conexao, $query);
-
-        $cargos = array();
-
-        while($cargo = mysqli_fetch_assoc($resultado))
-        {
-            array_push($cargos,$cargo);
+        if(count($cargo)==0){
+            $dao->fecharConexao();
+            return NULL;
         }
-
-        return $cargos;
+        $dao->fecharConexao();
+        return $cargo;
     }
 }
